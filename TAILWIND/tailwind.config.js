dad77bf9201879c844a7339d63242b209e8063e9
ManuelGuerraRuiz <mguerui@adaits.es>
2023-01-16 19:16:14 +0100

    actual

--- TAILWIND/tailwind.config.js
+++ TAILWIND/tailwind.config.js
@@ -1,8 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './components/**/*.{html,js}',
-      './pages/**/*.{html,js}',
-      './index.html',
-    ],
-    // ...
+  content: [ './components/**/*.{html,js}',
+  './pages/**/*.{html,js}',
+  './index.html',],
+  variants: {
+  extend: { 
+    animation:['responsive', 'hover', 'group-hover'],
+    fontSize:['responsive', 'hover', 'group-hover'],
+    transform:['responsive', 'hover', 'group-hover'],
+    scale:['responsive', 'hover', 'group-hover']
   }
+},
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
